b4d71f280b6b3dbc61baf805f3060c7d700522bd
nicolayreptile <timofey.abonosimov@gmail.com>
2020-09-14 01:16:14 +0300

    [add] add deploy files

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.6
+
+RUN mkdir /code
+
+COPY . /code
+
+RUN pip install -r /code/requirements.txt
+
+WORKDIR /code
+
+RUN python manage.py collectstatic --noinput
+RUN python manage.py makemigrations --noinput
+RUN python manage.py migrate --noinput
+
+CMD gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
+LABEL author=timofey_abonosimov
--- foodgram/settings.py
+++ foodgram/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
+from os import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,9 +92,13 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ.get('DB_NAME'),
+        'USER': environ.get('DB_USER'),
+        'PASSWORD': environ.get('DB_PASSWORD'),
+        'HOST': environ.get('DB_HOST'),
+        'PORT': environ.get('DB_PORT'),
+    },
 }
 
 
